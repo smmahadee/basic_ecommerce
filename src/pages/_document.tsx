2ca955c20fa7826dd1cce78fe1a2b28303ad5180
mahadee <mahadee@amharctech.com>
2023-08-20 22:37:48 +0600

    viewport meta tag removed from _document.js

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head>
-        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <meta
           name='description'
           content='This is a simple mini ecommerce project'
